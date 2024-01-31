5e0423fb126a492d9e745ac0c5539a59562f0bcd
Sylviachelimo <chelimosylvia280@gmail.com>
2024-01-31 15:47:47 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+// findMatching: Case-insensitive search for matching drivers' names
+function findMatching(drivers, query) {
+    return drivers.filter(driver =>
+      driver.toLowerCase() === query.toLowerCase()
+    );
+  }
+  
+  // fuzzyMatch: Search for drivers whose names begin with provided letters
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver =>
+      driver.toLowerCase().startsWith(letters.toLowerCase())
+    );
+  }
+  
+  // matchName: Search for drivers whose name property matches the provided string
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase());
+  }
+  
